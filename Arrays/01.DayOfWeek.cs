8434544208edf447e5a5c22e52097339883728a9
Albert Khurshudyan <albert.khurshudyan96@gmail.com>
2020-10-13 22:43:05 +0300

    initial commit

---
+++ Arrays/01.DayOfWeek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] DaysOfWeek = new string[7] { "Monday","Thuesday","Wednesday","Thursday","Friday","Saturday","Sunday" };
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (!(num > 0 && num < 8))
+            {
+                Console.WriteLine("Indalid Day!");
+                return;
+            }
+
+            Console.WriteLine(DaysOfWeek[num-1]);
+        }
+    }
+}
---
+++ Lists/05.SortNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine().Split().Select(decimal.Parse).ToList();
+
+            numbers.Sort();
+
+            Console.WriteLine(string.Join(" <= ", numbers));
+        }
+    }
+}
