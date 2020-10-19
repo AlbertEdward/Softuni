b70497e9f3176a8476cca44b2316bc0af3b18208
Albert Khurshudyan <albert.khurshudyan96@gmail.com>
2020-10-19 23:22:07 +0300

    Exercises

---
+++ Fundamentals/Arrays/06.ExtractMiddle12or3Elements/06.ExtractMiddle12or3Elements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ExtractMiddle12or3Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (nums.Length == 1)
+            {
+                System.Console.WriteLine($"{{ {nums[0]} }}");  
+            }
+            else if (nums.Length % 2 == 0)
+            {
+                System.Console.WriteLine($"{{ {nums[(nums.Length/2) -1]}, {nums[(nums.Length/2)]} }}");  
+               
+            }
+            else if (nums.Length % 2 == 1)
+            {
+                System.Console.WriteLine($"{{ {nums[(nums.Length/2) -1]}, {nums[(nums.Length/2)]}, {nums[((nums.Length/2) + 1)]} }}");  
+            }
+            
+        }
+    }
+}
