69929fabf42d2ac0f93de03bbdc5227718a5fdbe
Albert Khurshudyan <albert.khurshudyan96@gmail.com>
2020-11-11 23:07:31 +0200

    update

---
+++ Fundamentals/test/1.Zadacha/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1.Zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                a++;
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("end");
+        }
+    }
+}
