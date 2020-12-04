b15589329ffb05cc35192ec17510a99b9580a539
user <albert.edward96@gmail.com>
2020-12-04 16:14:16 +0200

    Exams

---
+++ Fundamentals/MidExamPreparation/02.ShoppingList/02.ShoppingList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = Console.ReadLine()
+               .Split("!")
+               .ToList();
+
+            List<string> command = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            while (command[0] != "Go" && command[1] != "Shopping!")
+            {
+                if (command[0] == "Urgent")
+                {
+                    if (!(shoppingList.Contains(command[1])))
+                    {
+                        shoppingList.Insert(0, command[1]);
+                    }
+
+                }
+                else if (command[0] == "Unnecessary")
+                {
+                    shoppingList.Remove(command[1]);
+                }
+                else if (command[0] == "Correct")
+                {
+                    if (shoppingList.Contains(command[1]))
+                    {
+                        int index = shoppingList.IndexOf(command[1]);
+                        shoppingList.RemoveAt(index);
+                        shoppingList.Insert(index, command[2]);
+                    }
+                }
+                else if (command[0] == "Rearrange")
+                {
+                    string item = command[1];
+                    int index = shoppingList.IndexOf(item);
+                    
+                    if(index >= 0)
+                    {
+                        shoppingList.RemoveAt(index);
+                        shoppingList.Add(item);
+                    }
+                }
+
+                command = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            }
+
+            Console.WriteLine(string.Join(", ", shoppingList));
+        }
+    }
+}
---
+++ Fundamentals/MidExamPreparation/03.HeartDelivery/03.HeartDelivery.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.HeartDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> neighborhood = Console.ReadLine()
+                .Split("@")
+                .Select(int.Parse)
+                .ToList();
+
+            List<string> commands = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            int Cupid = 0;
+
+            while (commands[0] != "Love!")
+            {
+                if (commands[0] == "Jump")
+                {
+                    Cupid += int.Parse(commands[1]);
+               
+                    if (Cupid >= neighborhood.Count)
+                    {
+                        Cupid = 0;
+                    }
+
+                    neighborhood[Cupid] -= 2;
+
+                    if (neighborhood[Cupid] == 0)
+                    {
+                        Console.WriteLine($"Place {Cupid} has Valentine's day.");
+                    }
+                    else if (neighborhood[Cupid] < 0)
+                    {
+                        Console.WriteLine($"Place {Cupid} already had Valentine's day.");
+                    }
+                }
+
+                commands = Console.ReadLine()
+                .Split()
+                .ToList();
+            }
+
+            Console.WriteLine($"Cupid's last position was {Cupid}.");
+
+            int flag = 0;
+
+            for (int i = 0; i < neighborhood.Count; i++)
+            {
+                if (neighborhood[i] > 0)
+                {
+                    flag++;
+                }
+            }
+
+            if (flag == 0)
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                Console.WriteLine($"Cupid has failed {flag} places.");
+            }
+        }
+    }
+}
