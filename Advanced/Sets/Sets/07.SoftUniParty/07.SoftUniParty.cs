2eef405c2d042fde2b3957f084cff91d88e5792c
user <albert.edward96@gmail.com>
2021-02-07 00:57:35 +0200

    Sets - Exercises

---
+++ Advanced/Sets/Sets/07.SoftUniParty/07.SoftUniParty.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reservationNumbers = Console.ReadLine();
+
+            HashSet<string> guests = new HashSet<string>();
+
+            while (reservationNumbers != "PARTY")
+            {
+                guests.Add(reservationNumbers);
+                reservationNumbers = Console.ReadLine();
+            }
+
+            reservationNumbers = Console.ReadLine();
+
+            while (reservationNumbers != "END")
+            {
+
+                if (guests.Contains(reservationNumbers))
+                {
+                    guests.Remove(reservationNumbers);
+                }
+
+                reservationNumbers = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Count);
+
+            foreach (var guest in guests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
