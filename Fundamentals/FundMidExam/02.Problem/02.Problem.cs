12cc6f2e846d28242bc2003a0d94af1275d8dc32
user <albert.edward96@gmail.com>
2021-02-27 16:49:54 +0200

    * 02.Problem.cs: MidExam27Feb
    * 03.Problem.cs:
    * 02.Problem.csproj:
    * 03.Problem.csproj:

---
+++ Fundamentals/FundMidExam/02.Problem/02.Problem.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _02.Problem
+{
+    class Program
+    {
+        /* ha|Do|mm|om|er
+         * Move Right 0
+         * Move Left 3
+         * Check Odd
+         * Move Left 2
+         * Move Left 10
+         * Move Left 0
+        */
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("|");
+
+            string[] commandsInput = Console.ReadLine().Split();
+
+            string command = commandsInput[0];
+
+            while (command != "Done")
+            {
+                if (command == "Move")
+                {
+                    string subCommand = commandsInput[1];
+                    int index = int.Parse(commandsInput[2]);
+                    if (subCommand == "Right")
+                    {
+                        int newIndex = index + 1;
+                        if (newIndex >= 0 && newIndex < input.Length && (index >= 0 || index < input.Length))
+                        {
+                            var swap = input[index];
+                            input[index] = input[newIndex];
+                            input[newIndex] = swap;
+                        }
+
+                    }
+                    else if (subCommand == "Left")
+                    {
+                        int newIndex = index - 1;
+                        if (newIndex >= 0 && newIndex < input.Length && (index >= 0 || index < input.Length))
+                        {
+                            var swap = input[index];
+                            input[index] = input[newIndex];
+                            input[newIndex] = swap;
+                        }
+
+                    }
+                }
+                else if (command == "Check")
+                {
+                    string subCommand = commandsInput[1];
+                    if (subCommand == "Even")
+                    {
+                        string result = "";
+                        for (int i = 0; i < input.Length; i += 2)
+                        {
+                            result += input[i] + " ";
+                        }
+                        Console.WriteLine(result.Trim());
+
+                    }
+                    else if (subCommand == "Odd")
+                    {
+                        string result = "";
+                        for (int i = 1; i < input.Length; i += 2)
+                        {
+                            result += input[i] + " ";
+                        }
+                        Console.WriteLine(result.Trim());
+                    }
+                }
+
+                commandsInput = Console.ReadLine().Split();
+                command = commandsInput[0];
+
+            }
+
+            Console.WriteLine($"You crafted {String.Join("",input)}!");
+        }
+    }
+}
---
+++ Fundamentals/FundMidExam/03.Problem/03.Problem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> allCards = Console.ReadLine().Split(":").ToList();
+
+            List<string> newDeck = new List<string>();
+
+            string[] commands = Console.ReadLine().Split();
+            string command = commands[0];
+
+            while (command != "Ready")
+            {
+                if (command == "Add")
+                {
+                    if (allCards.Contains(commands[1]))
+                    {
+                        newDeck.Add(commands[1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Card not found.");
+                    }
+                    
+                }
+                else if (command == "Insert")
+                {
+                    int hui = int.Parse(commands[2]);
+
+                    if (allCards.Contains(commands[1]) && hui >= 0 && hui < newDeck.Count)
+                    {
+                        newDeck.Insert(int.Parse(commands[2]), commands[1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    
+                }
+                else if (command == "Remove")
+                {
+                    if (newDeck.Contains(commands[1]))
+                    {
+                        newDeck.Remove(commands[1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Card not found.");
+                    }
+                }
+                else if (command == "Swap")
+                {
+                    var firstIndex = newDeck.IndexOf(commands[1]);
+                    var secondIndex = newDeck.IndexOf(commands[2]);
+
+                    if(firstIndex<0 || secondIndex<0) {continue;}
+
+
+                    var swap = newDeck[firstIndex];
+                    newDeck[firstIndex] = newDeck[secondIndex];
+                    newDeck[secondIndex] = swap;
+                }
+                else if (command == "Shuffle")
+                {
+                    newDeck.Reverse();
+                }
+
+                commands = Console.ReadLine().Split();
+                command = commands[0];
+            }
+
+            Console.WriteLine(string.Join(" ", newDeck));
+        }
+    }
+}
