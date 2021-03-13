a7f605af0fd9d11d47e5cc390dc6c455cb14a48e
user <albert.edward96@gmail.com>
2021-03-13 21:02:21 +0200

    Exersices

---
+++ Fundamentals/TextProcessing/05.DigitsLettersAndOther/05.DigitsLettersAndOther.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace DigitsLettersAndOther
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string digits = "";
+            string letters = "";
+            string others = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text, i))
+                {
+                    digits += text[i];
+                }
+                else if (char.IsLetter(text, i))
+                {
+                    letters += text[i];
+                }
+                else
+                {
+                    others += text[i];
+                }
+                
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
