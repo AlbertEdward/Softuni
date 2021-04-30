40918ee5dfb5aa711677dbe74a6877c7f4035772
user <albert.edward96@gmail.com>
2021-05-01 00:50:01 +0300

    Problems

---
+++ Advanced/StreamsFilesAndDirectories/StreamsAndFiles/02.Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
