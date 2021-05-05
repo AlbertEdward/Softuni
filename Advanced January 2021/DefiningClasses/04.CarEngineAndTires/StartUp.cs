a689863c0e66426b65ff7a96a21b035f3da592f7
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-05-05 22:45:08 +0300

    Tasks

---
+++ Advanced January 2021/DefiningClasses/04.CarEngineAndTires/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine V6 = new Engine(500, 1000);
+
+            Tire[] Pirelli = new Tire[]
+            {
+                new Tire(2020, 2.0),
+                new Tire(2018, 2.5),
+                new Tire(2029, 2.2),
+                new Tire(2020, 2.3),
+            };
+
+            Car CLK = new Car("Mercedes", "W209", 2003, 60, 17, V6, Pirelli);
+
+            Console.WriteLine("HorsePower: " + CLK.Engine.HorsePower);
+
+            foreach (var tire in CLK.Tires)
+            {
+                Console.WriteLine($"{tire.Year} - {tire.Pressure}");
+            }
+        }
+    }
+}
---
+++ Advanced January 2021/DefiningClasses/05.SpecialCars/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "No more tires")
+            {
+                string[] tireInfo = Console.ReadLine().Split();
+
+                new Tire(int.Parse(tireInfo[0]), double.Parse(tireInfo[1]));
+
+                command = Console.ReadLine();
+            }
+
+            while (command != "Engines done")
+            {
+                string[] engineInfo = Console.ReadLine().Split();
+
+                new Engine(int.Parse(engineInfo[0]), double.Parse(engineInfo[1]));
+
+                command = Console.ReadLine();
+            }
+            
+            while (command != "Show special")
+            {
+                string[] carsInfo = Console.ReadLine().Split();
+
+
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
