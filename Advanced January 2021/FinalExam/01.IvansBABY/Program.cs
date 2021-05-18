b1a63d95fd138eb04497a1cad0c7588b7cf2ed98
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-05-18 23:33:18 +0300

    Tasks

---
+++ Advanced January 2021/FinalExam/01.IvansBABY/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01.IvansBABY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is my baby sex:  ");
+            string babyType = Console.ReadLine();
+            int girls = 0;
+            int gender = 0;
+            while (babyType != "boy" || babyType != "boy")
+            {
+                if (babyType == "girl" || babyType == "Girl")
+                {
+                    Console.WriteLine("Try again!");
+                    girls++;
+                }
+                else if (babyType == "gender" || babyType == "Gender")
+                {
+                    Console.WriteLine("Мамата си е ебало");
+                    gender++; 
+                }
+                
+                Console.Write("Enter baby sex: ");
+                babyType = Console.ReadLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Mission Complete!");
+            Console.WriteLine($"You have total {girls + 1 + gender} kids.");
+            Console.WriteLine($"Girls -> {girls}");
+            Console.WriteLine($"Genders -> {gender}");
+            Console.WriteLine("Boys -> 1");
+            
+        }
+    }
+}
--- Advanced January 2021/FinalExam/02.Bee/Program.cs
+++ Advanced January 2021/FinalExam/02.Bee/Program.cs
@@ -22,6 +22,8 @@ namespace _02.Bee
 
 
 
+
+
                 }
             }
             
