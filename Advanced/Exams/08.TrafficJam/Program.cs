ced8fd793c3db0fc7f613f9b6503eb984a193139
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-05-20 23:23:48 +0300

    Tasks

---
+++ Advanced/Exams/08.TrafficJam/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            Queue<string> passedCars = new Queue<string>();
+
+            int count = 0;
+
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    if (passedCars.Count % n == 0)
+                    {
+                        foreach (var item in passedCars)
+                        {
+                            Console.WriteLine(item + " passed!");
+                            count++;
+                        }
+
+                        passedCars.Clear();
+                    }
+                    else
+                    {
+                        int c = passedCars.Count - (passedCars.Count / n);
+
+                        for (int i = 0; i < c; i++)
+                        {
+                            Console.WriteLine(passedCars.Dequeue() + " passed!");
+                            count++;
+
+                        }
+                    }
+                }
+                else
+                {
+                    passedCars.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+
+            Console.WriteLine(count + " cars passed the crossroads.");
+        }
+    }
+}
