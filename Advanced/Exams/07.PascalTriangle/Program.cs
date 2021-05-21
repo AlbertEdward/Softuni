0e538a7f3465d9718a52c56766eff9ed48f529ee
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-05-21 22:55:39 +0300

    Tasks

---
+++ Advanced/Exams/07.PascalTriangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] one = new int[n,n];
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    one[row, col] = ;
+                }
+            }
+        }
+    }
+}
