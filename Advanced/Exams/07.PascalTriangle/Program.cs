e7c34d3c116d71459812126b3da28b4c889ebd58
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-05-22 21:59:25 +0300

    Tasks

--- Advanced/Exams/07.PascalTriangle/Program.cs
+++ Advanced/Exams/07.PascalTriangle/Program.cs
@@ -7,15 +7,24 @@ namespace _07.PascalTriangle
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            int[][] triangle = new int[n][];
 
-            int[,] one = new int[n,n];
+            triangle[0][0] = 1;
 
-            for (int row = 0; row < n; row++)
+            for (int row = 0; row < n - 1; row++)
             {
-                for (int col = 0; col < n; col++)
+                for (int col = 0; col <= row; col++)
                 {
-                    one[row, col] = ;
+                    triangle[row + 1][col] += triangle[row][col];
+                    triangle[row + 1][col + 1] += triangle[row][col];
                 }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(triangle[i]);
+                Console.WriteLine();
+
             }
         }
     }
