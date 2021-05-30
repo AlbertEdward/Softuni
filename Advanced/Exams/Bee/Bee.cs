ced893c889669e53b52bc7524945cfe810f1b17a
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-05-30 19:05:43 +0300

    Tasks

--- Advanced/Exams/Bee/Bee.cs
+++ Advanced/Exams/Bee/Bee.cs
@@ -10,59 +10,123 @@ namespace Bee
         {
             int teritory = int.Parse(Console.ReadLine());
             string[,] beeTeritory = new string[teritory, teritory];
-
-            for (int row = 0; row < teritory; row++)
-            {
-                string put = Console.ReadLine()
-                    .Split("")
-                    .ToString();
-
-                for (int col = 0; col < teritory; col++)
-                {
-                    beeTeritory[row, col] = ;
-                }
-            }
-            
             int beeRow = 0;
             int beeCol = 0;
 
-            int pollinated = 0;
-
-            for (int i = 0; i < teritory; i++)
+            for (int row = 0; row < teritory; row++)
             {
-                for (int j = 0; j < teritory; j++)
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < teritory; col++)
                 {
-                    if (beeTeritory[i, j] == "B")
+                    beeTeritory[row, col] = input[col].ToString();
+
+                    if (beeTeritory[row, col] == "B")
                     {
-                        beeRow = i;
-                        beeCol = j;
-                        break;
+                        beeRow = row;
+                        beeCol = col;
                     }
                 }
             }
 
+            int pollinated = 0;
+
             string commands = Console.ReadLine();
 
-            if (commands == "left")
+            while (commands != "End")
             {
-                beeCol--;
-            }
-            else if (commands == "right")
-            {
-                beeCol++;
-            }
-            else if (commands == "up")
-            {
-                beeRow--;
-            }
-            else if (commands == "down")
-            {
-                beeRow--;
+                beeTeritory[beeRow, beeCol] = ".";
+
+                if (commands == "left")
+                {
+                    beeCol--;
+                }
+                else if (commands == "right")
+                {
+                    beeCol++;
+                }
+                else if (commands == "up")
+                {
+                    beeRow--;
+                }
+                else if (commands == "down")
+                {
+                    beeRow++;
+                }
+
+                bool isValid = Check(beeCol,beeRow,teritory);
+
+                if (!isValid)
+                {
+                    Console.WriteLine("The bee got lost!");
+                    break;
+                }
+
+                string checkIndex = beeTeritory[beeRow, beeCol];
+
+                if (checkIndex == "f")
+                {
+                    pollinated++;
+                }
+                else if (checkIndex == "O")
+                {
+                    beeTeritory[beeRow, beeCol] = ".";
+
+                    if (commands == "left")
+                    {
+                        beeCol--;
+                    }
+                    else if (commands == "right")
+                    {
+                        beeCol++;
+                    }
+                    else if (commands == "up")
+                    {
+                        beeRow--;
+                    }
+                    else if (commands == "down")
+                    {
+                        beeRow++;
+                    }
+
+                    checkIndex = beeTeritory[beeRow, beeCol];
+
+                    if (checkIndex == "f")
+                    {
+                        pollinated++;
+                    }
+                }
+                beeTeritory[beeRow, beeCol] = "B";
+                commands = Console.ReadLine();
+
             }
 
-            
+            if (pollinated < 5)
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - pollinated} flowers more");
+            }
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {pollinated} flowers!");
+            }
 
+            int count = 0;
 
+            foreach (var item in beeTeritory)
+            {
+                Console.Write(item);
+                count++;
+                if (count >= teritory)
+                {
+                    Console.WriteLine();
+                    count = 0;
+                }
+            }
+        }
+
+        private static bool Check(int beeCol, int beeRow, int teritory)
+        {
+            return beeCol >= 0 && beeCol < teritory && beeRow >= 0 && beeRow < teritory;
         }
     }
 }
