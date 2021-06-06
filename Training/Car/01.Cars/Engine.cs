3c285e22346e1e93defb6721909577013ab5be6f
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-06-07 00:53:06 +0300

    Class

---
+++ Training/Car/01.Cars/Engine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        private int horsePorwer;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+    }
+}
