df258c6f2898ebffbb4cb06e51d8be90f0fb430a
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-06-11 21:48:48 +0300

    Tasks

---
+++ OOP/Encapsulation/01.SortPersonsByNameAndAge/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team : Person
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+
+        }
+    }
+}
