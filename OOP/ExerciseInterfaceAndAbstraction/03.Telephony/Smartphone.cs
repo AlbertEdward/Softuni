88c7e54841c0269cf3944e2a97ceee8f87f938d5
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-06-18 21:14:54 +0300

    Changes made on Fri Jun 18 21:14:54 EEST 2021

--- OOP/ExerciseInterfaceAndAbstraction/03.Telephony/Smartphone.cs
+++ OOP/ExerciseInterfaceAndAbstraction/03.Telephony/Smartphone.cs
@@ -20,6 +20,10 @@ namespace ManufacturingPhones
 
 
                     }
+                    else
+                    {
+
+                    }
                 }
                 Console.WriteLine($"Dialing... {Number}");
             }
