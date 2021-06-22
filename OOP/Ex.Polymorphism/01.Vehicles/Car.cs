393b5f51a341c16a3e383eacf180e30fc35fa620
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-06-22 19:29:19 +0300

    Changes made on Tue Jun 22 19:29:19 EEST 2021

---
+++ OOP/Ex.Polymorphism/01.Vehicles/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace _01.Vehicles
+{
+    public class Car
+    {
+        public Car(double fuelQuantity, double fuelConsumption, double distance, object liters)
+        {
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+            this.distance = distance;
+            this.liters = liters;
+        }
+
+        public double fuelQuantity { get; set; }
+        public double fuelConsumption { get; set; }
+        public double distance { get; set; }
+        public object liters { get; set; }
+    }
+}
--- OOP/Ex.Polymorphism/01.Vehicles/StartUp.cs
+++ OOP/Ex.Polymorphism/01.Vehicles/StartUp.cs
@@ -2,7 +2,7 @@
 
 namespace _01.Vehicles
 {
-    class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
