5a24e2066e3e43b53c906a18e520ee8068c150b2
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-06-23 22:24:05 +0300

    Changes made on Wed Jun 23 22:24:05 EEST 2021

---
+++ Advanced/ExamPreparation/01.Scheduling/01.Scheduling.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Scheduling
+{
+    class Scheduling
+    {
+        static void Main(string[] args)
+        {
+            List<int> tasks = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+            List<int> threads = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int needToKill = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < threads.Count; i++)
+            {
+                if (tasks[i] == needToKill)
+                { 
+                    Console.WriteLine($"Thread with value {threads[i]} killed task {needToKill}");
+                    break;
+                }
+                if (threads[i] > tasks[i] )
+                {
+                    threads.RemoveAt(i);
+                    tasks.RemoveAt(i);
+                    i--;
+                }
+                else if (threads[i] < tasks[i])
+                {
+                    threads.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", threads));
+
+        }
+    }
+}
---
+++ Advanced/ExamPreparation/02.Garden/02.Garden.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Garden
+{
+    class Garden
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                for (int l = 0; l < dimensions[1]; l++)
+                {
+                    matrix[i, l] = 0;
+                }
+            }
+
+            string commands = Console.ReadLine();
+
+            while (commands != "Bloom Bloom Plow")
+            {
+                string[] task = commands.Split().ToArray();
+
+                int row = int.Parse(task[0]);
+                int col = int.Parse(task[1]);
+
+                if (row > dimensions[0] || col > dimensions[1] || row < 0 || col < 0)
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    commands = Console.ReadLine();
+                    continue;
+                }
+
+                for (int i = 0; i < dimensions[0]; i++)
+                {
+                    if (matrix[row,i] == 0)
+                    {
+                        matrix[row, i] = 1;
+                    }
+                    else
+                    {
+                        matrix[row, i] += 1;
+                    }
+                }
+                for(int j = 0; j < dimensions[1]; j++)
+                {
+                    if (matrix[j,col] == 0)
+                    {
+                        matrix[j, col] = 1;
+                    }
+                    else
+                    {
+                        matrix[j, col] += 1;
+                    }
+                }
+
+                matrix[row, col] = 1;
+
+                commands = Console.ReadLine();
+            }
+
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                for (int k = 0; k < dimensions[1]; k++)
+                {
+                    Console.Write(matrix[i,k] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
