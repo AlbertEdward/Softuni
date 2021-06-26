33d934a12c03eefdc65527ebefee0e0540374816
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-06-26 12:49:51 +0300

    Changes made on Sat Jun 26 12:49:51 EEST 2021

---
+++ Advanced/FinalExam/01.Problem/01.Masterchef.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Masterchef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ingredient = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> freshness = Console.ReadLine().Split().Select(int.Parse).Reverse().ToList();
+
+            int levelFreshness = 0;
+            int dippingSauce = 0;
+            int greenSalad = 0;
+            int chocolateCake = 0;
+            int Lobster = 0;
+
+            int max = Math.Min(ingredient.Count, freshness.Count);
+
+            for (int i = 0; i < max; i++)
+            {
+                levelFreshness = ingredient[i] * freshness[i];
+
+                if (levelFreshness == 0)
+                {
+                    ingredient.Remove(ingredient[i]);
+                    i--;
+                    max++;
+                    continue;
+                }
+                else if (levelFreshness == 150)
+                {
+                    dippingSauce++;
+                    ingredient.Remove(ingredient[i]);
+                    freshness.Remove(freshness[i]);
+                    i--;
+                }
+                else if (levelFreshness == 250)
+                {
+                    greenSalad++;
+                    ingredient.Remove(ingredient[i]);
+                    freshness.Remove(freshness[i]);
+                    i--;
+                }
+                else if (levelFreshness == 300)
+                {
+                    chocolateCake++;
+                    ingredient.Remove(ingredient[i]);
+                    freshness.Remove(freshness[i]);
+                    i--;
+                }
+                else if (levelFreshness == 400)
+                {
+                    Lobster++;
+                    ingredient.Remove(ingredient[i]);
+                    freshness.Remove(freshness[i]);
+                    i--;
+                }
+                else
+                {
+                    freshness.Remove(freshness[i]);
+
+                    int saver = ingredient[i] + 5;
+
+                    ingredient.Remove(ingredient[i]);
+
+                    ingredient.Insert(ingredient.Count, saver);
+
+                    i--;
+                }
+
+                max = Math.Min(ingredient.Count, freshness.Count);
+            }
+
+            if (dippingSauce > 0 &&  greenSalad > 0 && chocolateCake > 0 && Lobster > 0)
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+                Console.WriteLine($" # Chocolate cake --> {chocolateCake}");
+                Console.WriteLine($" # Dipping sauce --> {dippingSauce}");
+                Console.WriteLine($" # Green salad --> {greenSalad}");
+                Console.WriteLine($" # Lobster --> {Lobster}");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+
+                if (ingredient.Sum() > 0)
+                {
+                    Console.WriteLine($"Ingredients left: {ingredient.Sum()}");
+                }
+
+
+                if (chocolateCake > 0)
+                {
+                    Console.WriteLine($" # Chocolate cake --> {chocolateCake}");
+                }
+                if (dippingSauce > 0)
+                {
+                    Console.WriteLine($" # Dipping sauce --> {dippingSauce}");
+                }
+                if (greenSalad > 0)
+                {
+                    Console.WriteLine($" # Green salad --> {greenSalad}");
+                }
+                if (Lobster > 0)
+                {
+                    Console.WriteLine($" # Lobster --> {Lobster}");
+                }
+            }
+
+           
+        }
+    }
+}
---
+++ Advanced/FinalExam/02.Problem/Survivor.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[][] jagged = new string[n][];
+
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                string[] inputNumbers = Console.ReadLine().Split();
+
+                jagged[row] = new string[inputNumbers.Length];
+
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    jagged[row][col] = inputNumbers[col];
+                }
+            }
+
+            string[] commands = Console.ReadLine().Split().ToArray();
+
+            int tokens = 0;
+            int opponentTokens = 0;
+
+            while (commands[0] != "Gang")
+            {
+                int row = int.Parse(commands[1]);
+                int col = int.Parse(commands[2]);
+
+
+                if (row > n || row < 0)
+                {
+
+                }
+                else if (jagged[row].Length < col || jagged.Length < 0)
+                {
+
+                }
+
+                if (commands[0] == "Find")
+                {
+
+                    if (jagged[row][col] == "T")
+                    {
+                        tokens++;
+                        jagged[row][col] = "-";
+                    }
+
+                }
+                else if (commands[0] == "Opponent")
+                {
+                    if (jagged[row][col] == "T")
+                    {
+                        opponentTokens++;
+                        jagged[row][col] = "-";
+                    }
+
+                    if (commands[3] == "up")
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            int a = row + i;
+                            if (a >= 0)
+                            {
+                                if (jagged[row - i][col] == "T")
+                                {
+                                    opponentTokens++;
+                                    jagged[row][col] = "-";
+                                }
+                            }
+
+                        }
+
+                    }
+                    else if (commands[3] == "down")
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            int a = row - i;
+                            if (a <= n)
+                            {
+                                if (jagged[row + i][col] == "T")
+                                {
+                                    opponentTokens++;
+                                    jagged[row][col] = "-";
+                                }
+                            }
+
+                        }
+                    }
+                    else if (commands[3] == "left")
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            int a = col - i;
+                            if (a >= jagged[row].Length)
+                            {
+                                if (jagged[row][col-i] == "T")
+                                {
+                                    opponentTokens++;
+                                    jagged[row][col] = "-";
+                                }
+                            }
+
+                        }
+
+
+                    }
+                      else if (commands[3] == "right")
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            int a = col + i;
+                            if (a <= jagged[row].Length)
+                            {
+                                if (jagged[row][col+i] == "T")
+                                {
+                                    opponentTokens++;
+                                    jagged[row][col] = "-";
+                                }
+                            }
+
+                        }
+
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
