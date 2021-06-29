e8a1056ebc5450855435d4dd72e7c77d76f05358
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-06-29 22:46:03 +0300

    Changes made on Tue Jun 29 22:46:03 EEST 2021

---
+++ OOP/Inheritance/InheritanceEx./01.Person/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace _01.Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Gei");
+                }
+
+                age = value;           
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format("Name: {0}, Age {1}", this.name, this.age));
+
+            return sb.ToString();
+        }
+
+    }   
+}
---
+++ OOP/ReusingClasses/04.RandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.RandomList
+{
+    public class RandomList
+    {
+        private data = new List<string>();
+
+        public List<string> data { get; set; }
+
+        public string RandomString()
+        {
+            var random = new Random();C
+
+            string index = random.Next(Data.Count);
+
+            return index;
+        }
+    }
+}
