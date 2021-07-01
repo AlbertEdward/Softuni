a2382dbdec889c8066317837c6601259185ced14
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-07-01 23:57:06 +0300

    Changes made on Thu Jul  1 23:57:06 EEST 2021

---
+++ OOP/Encapsulations/PersonsInfo/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace PersonsInfo
+{
+    public class Person
+    { 
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age > 30)
+            {
+                Salary += percentage * Salary / 100;
+            }
+            else
+            {
+                Salary += percentage * Salary / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receive {Salary:f2} leva.";
+        }
+    }
+}
