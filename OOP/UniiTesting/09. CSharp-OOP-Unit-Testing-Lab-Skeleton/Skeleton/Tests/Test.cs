c568d3a88a49057c0b15552b08fa94a3b88fa8b7
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-07-21 00:09:18 +0300

    Unit Testing

---
+++ OOP/UniiTesting/09. CSharp-OOP-Unit-Testing-Lab-Skeleton/Skeleton/Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace Tests
+{
+    [TestFixture()]
+    public class AxeTests
+    {
+        [Test()]
+        public void AxeLoosesDurabilityAfterAttack()
+        {
+            Axe 
+        } 
+    }
+}
