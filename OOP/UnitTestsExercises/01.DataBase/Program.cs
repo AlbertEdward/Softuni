ef34c8ca0381e4f223c6e11cf8d6d854851eb0eb
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-07-22 23:03:02 +0300

    Changes made on Thu Jul 22 23:03:02 EEST 2021

---
+++ OOP/UnitTestsExercises/01.DataBase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataBase
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ OOP/UnitTestsExercises/DatabaseTests/DataBaseTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DatabaseTests
+{
+    public class Test
+    {
+        private Database.Database database;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.database = new Database.Database();
+        }
+
+        [Test]
+        public void CheckArrayThatIsItEqualTo16()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                database.Add(i);
+            }
+            Assert.That(database.Count, Is.EqualTo(16));
+        }
+
+
+        [Test]
+        public void Add_ThrowException_WhenCapacityExceeded()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                database.Add(i);
+            }
+
+            Assert.Throws<InvalidOperationException>(() => database.Add(17));
+        }
+
+        [Test]
+        public void Remove_()
+        {
+            database.Remove();
+
+            Assert.Throws<InvalidOperationException>(() => database.Remove());
+        }
+    }
+} 
