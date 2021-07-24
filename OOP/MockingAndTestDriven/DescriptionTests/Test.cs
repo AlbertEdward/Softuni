3bef78a7becf3d0d021bb226eb7532af98fcd1a8
AlbertEdward <albert.khurshudyan96@gmail.com>
2021-07-24 23:51:22 +0300

    Changes made on Sat Jul 24 23:51:22 EEST 2021

---
+++ OOP/MockingAndTestDriven/DescriptionTests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace DescriptionTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
