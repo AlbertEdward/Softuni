18d49705eb30fd2ea51633a48d8455ebd28a8d5f
AlbertEdward <albert.khurshudyan96@gmail.com>
2022-01-18 20:11:42 +0200

    basic web

---
+++ C# Web/BasicWebServer/BasicWebServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BasicWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
+            var serverListener = new TcpListener(ipAddress, port);
+
+            serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}");
+            Console.WriteLine("Listening for requests...");
+
+            var connection = serverListener.AcceptTcpClient();
+
+        }
+    }
+}
+
+
---
+++ OOP/Exersises/Exersises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exersises
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
